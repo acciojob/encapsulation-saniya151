b9db7ee0d5526202cab155b5e7c0440663de972e
Syeda Saniya <syedasaniyaparveen1@gmail.com>
2023-08-27 11:16:40 -0700

    change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+        // Try setting a value directly (Task 4)
+        // rw.name = "John"; // This will give an error
+
+        // Try printing the value (Task 4)
+        // System.out.println(rw.name); // This will give an error
+
+        // Use setter to set the value (Task 6)
+        rw.setName("John");
+
+        // Use getter to access the value (Task 6)
+        System.out.println("Name: " + rw.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Setter method
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    // Getter method
+    public String getName() {
+        return name;
+    }
 }
